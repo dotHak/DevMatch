b51211a888eb7e2cd044d37eb5ad6d5b0953f4a1
Kwaku Hubert <kwakuhubert@gmail.com>
2018-09-22 12:08:46 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'shrouded-fortress-66409.herokuapp.com',
+  :domain         => 'devmatchtest.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
