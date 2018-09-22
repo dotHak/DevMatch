a5cd12fa5b9a39c7bec0a0bc808ed3a55a52c09a
Kwaku Hubert <kwakuhubert@gmail.com>
2018-09-22 18:46:29 +0000

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_22_175611) do
+ActiveRecord::Schema.define(version: 2018_09_22_183551) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_09_22_175611) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "plan_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
